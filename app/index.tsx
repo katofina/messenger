fd42d5bd88a6e2b16e634c087cb93e90f277f9ae
Lizaveta Ivanova <lizuha1860@gmail.com>
2024-09-10 14:27:50 +0300

    initial commit, master

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
+
+export default function initiallScreen() {
+  return (
+    <SafeAreaView>
+      <Text>Messenger</Text>
+    </SafeAreaView>
+  );
+};
